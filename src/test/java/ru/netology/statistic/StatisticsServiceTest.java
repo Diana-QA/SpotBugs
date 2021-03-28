6605cdd11b1334d15c069b34664c533d06d53bd9
Diana Krapivina <mr.ikoma@mail.ru>
2021-03-28 17:07:05 +0300

    SpotBugs added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxTwo() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxThree() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {24, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2};
+        long expected = 24;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
